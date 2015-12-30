e367990929e6e8cc74a06672e5947c903e813ec6
Phil Calcado <pcalcado@gmail.com>
2015-12-30 23:54:26 +0100

    Add comments

--- packages.go
+++ packages.go
@@ -6,24 +6,31 @@ import (
 	"strings"
 )
 
+//Represents a package and its dependencies
 type Package struct {
 	Name         string
 	Processed    bool
 	Dependencies []*Package
 }
 
+//Makes this package depend on some other
 func (pkg *Package) AddDependency(to *Package) {
 	pkg.Dependencies = append(pkg.Dependencies, to)
 }
 
+//A repository for all known packages
 type AllPackages struct {
+	//All packages we know of
 	Packages []*Package
 }
 
 var (
+	//Matches well-formed lines from the data file, see data.go
+	//and data/brew-dependencies.txt
 	lineMatcher, _ = regexp.Compile("^\\w+: ?(\\w+ *)*")
 )
 
+// Returns the names of all known packages
 func (allPackages *AllPackages) Names() []string {
 	names := make([]string, len(allPackages.Packages))
 	for _, p := range allPackages.Packages {
@@ -32,6 +39,9 @@ func (allPackages *AllPackages) Names() []string {
 	return names
 }
 
+// Finds or creates a package with given name. This should be the only
+// function used to instantiate packages in production so that we can
+// keep a single instance per package.
 func (allPackages *AllPackages) Named(name string) *Package {
 	var pkg *Package
 
@@ -49,6 +59,8 @@ func (allPackages *AllPackages) Named(name string) *Package {
 	return pkg
 }
 
+// Utility function to create a package. Should not be used directly
+// from production code, use AllPackages#Named()
 func MakeUnprocessedPackage(name string) *Package {
 	return &Package{
 		Name:         name,
@@ -57,6 +69,9 @@ func MakeUnprocessedPackage(name string) *Package {
 	}
 }
 
+// Parses a single line from the text file, returns the relevant
+// tokens as an array. The first element of the array is the package
+// name, any subsequent elements are dependencies.
 func ParsePackageFromLine(line string) (*Package, error) {
 	if !lineMatcher.MatchString(line) {
 		return nil, fmt.Errorf("Invalid line: %s", line)
