5b0a03cd8de5b67891553d458625f75cfac91fd7
Phil Calcado <pcalcado@gmail.com>
2016-01-04 01:36:19 +0100

    Create candidate package

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+artifact_dir=package
+candidate_package_dir=do-package-tree
+test_suite_dir=test-suite
+
+rm -rf $artifact_dir
+mkdir $artifact_dir
+
+rm -rf $candidate_package_dir
+mkdir $candidate_package_dir
+
+#compile test-suite
+pushd $test_suite_dir
+make
+popd
+
+#build package
+cp INSTRUCTIONS.md $candidate_package_dir/
+cp $test_suite_dir/do-package-tree  $candidate_package_dir/
+tar -cvzf $candidate_package_dir/source.tar.gz $test_suite_dir/*go
+
+tar -cvzf $artifact_dir/candidate.tar.gz $candidate_package_dir 
+
+rm -rf $candidate_package_dir
