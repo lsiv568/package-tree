bb0db5a6ef8d929121aaaf19d38df5420f971588
Phil Calcado <pcalcado@gmail.com>
2016-02-03 15:05:06 +0100

    Add more tests

--- test-suite/test_run.go
+++ test-suite/test_run.go
@@ -13,6 +13,7 @@ type TestRun struct {
 	ServerPort       int
 	StartedAt        time.Time
 	ConcurrencyLevel int
+	waiting          sync.WaitGroup
 }
 
 // Start starts the test
@@ -89,72 +90,43 @@ func (t *TestRun) Phase2() {
 	segmentedPackages := SegmentListPackages(homebrewPackages.Packages, t.ConcurrencyLevel)
 
 	clientCounter := 0
+	concurrentBruteforceRemovesAllPackages(clientCounter, t, segmentedPackages)
 
-	var waiting sync.WaitGroup
+	clientCounter = clientCounter + t.ConcurrencyLevel
+	concurrentBruteforceIndexesPackages(clientCounter, t, segmentedPackages)
 
-	waiting.Add(t.ConcurrencyLevel)
-	for _, p := range segmentedPackages {
-		clientCounter++
-		go func(number int, packagesToProcess []*Package) {
-			name := fmt.Sprintf("client[%d]", number+1)
-			log.Printf("Starting %s", name)
-			defer waiting.Done()
+	clientCounter = clientCounter + t.ConcurrencyLevel
+	concurrentVerifyAllPackages(clientCounter, t, segmentedPackages, OK)
 
-			client := makeClient(name, t)
-			defer client.Close()
+	clientCounter = clientCounter + t.ConcurrencyLevel
+	concurrentBruteforceRemovesAllPackages(clientCounter, t, segmentedPackages)
 
-			err = BruteforceRemovesAllPackages(client, packagesToProcess)
-			if err != nil {
-				t.Failf("%v", err)
-			}
-		}(clientCounter, p)
-	}
-	waiting.Wait()
-
-	waiting.Add(t.ConcurrencyLevel)
-	for _, p := range segmentedPackages {
-		clientCounter++
-		go func(number int, packagesToProcess []*Package) {
-			name := fmt.Sprintf("client[%d]", number+1)
-			log.Printf("Starting %s", name)
-			defer waiting.Done()
-
-			client := makeClient(name, t)
-			defer client.Close()
-
-			err = BruteforceIndexesPackages(client, packagesToProcess)
-			if err != nil {
-				t.Failf("%v", err)
-			}
-		}(clientCounter, p)
-	}
-	waiting.Wait()
+	clientCounter = clientCounter + t.ConcurrencyLevel
+	concurrentVerifyAllPackages(clientCounter, t, segmentedPackages, FAIL)
 
 	duration := time.Since(startedAt)
 	log.Printf("TESTRUN Phase2 - FINISHED (took %dms %v)", durationInMillis(duration), duration)
 
 }
 
-func durationInMillis(d time.Duration) int64 {
-	return d.Nanoseconds() / int64(time.Millisecond)
-}
-
 func BruteforceIndexesPackages(client PackageIndexerClient, packages []*Package) error {
 	totalPackages := len(packages)
-	log.Printf("Brute-forcing indexing of %d packages", totalPackages)
-	for installedPackages := 0; installedPackages < totalPackages; {
+	log.Printf("%s brute-forcing indexing of %d packages", client.Name(), totalPackages)
+	for numPackagesInstalledThisItearion := 0; numPackagesInstalledThisItearion < totalPackages; {
+		numPackagesInstalledThisItearion = 0
 		for _, pkg := range packages {
-			responseCode, err := client.Send(MakeIndexMessage(pkg))
+			msg := MakeIndexMessage(pkg)
+			responseCode, err := client.Send(msg)
 
 			if err != nil {
-				return fmt.Errorf("When reading: %v", err)
+				return fmt.Errorf("%s found error when sending message [%s]: %v", client.Name(), msg, err)
 			}
 
 			if responseCode == OK {
-				installedPackages = installedPackages + 1
+				numPackagesInstalledThisItearion = numPackagesInstalledThisItearion + 1
 			}
 		}
-		log.Printf("%v/%v packages indexed", installedPackages, totalPackages)
+		log.Printf("%s reports %v/%v packages indexed", client.Name(), numPackagesInstalledThisItearion, totalPackages)
 	}
 
 	return nil
@@ -185,16 +157,16 @@ func BruteforceRemovesAllPackages(client PackageIndexerClient, packages []*Packa
 
 func VerifyAllPackages(client PackageIndexerClient, packages []*Package, expectedResponseCode ResponseCode) error {
 	totalPackages := len(packages)
-	log.Printf("Querying for %d packages and expecting status code to be [%s]", totalPackages, expectedResponseCode)
+	log.Printf("%s querying for %d packages and expecting status code to be [%s]", client.Name(), totalPackages, expectedResponseCode)
 	for _, pkg := range packages {
-		responseCode, err := client.Send(MakeQueryMessage(pkg))
-
+		msg := MakeQueryMessage(pkg)
+		responseCode, err := client.Send(msg)
 		if err != nil {
-			return fmt.Errorf("When reading: %v", err)
+			return fmt.Errorf("%s found error when sending message [%s]: %v", client.Name(), msg, err)
 		}
 
 		if responseCode != expectedResponseCode {
-			return fmt.Errorf("Expected query for package [%s] to return [%s], got [%s]", pkg.Name, expectedResponseCode, responseCode)
+			return fmt.Errorf("%s expected query for package [%s] to return [%s], got [%s]", client.Name(), pkg.Name, expectedResponseCode, responseCode)
 		}
 	}
 
@@ -207,4 +179,71 @@ func makeClient(clientName string, t *TestRun) PackageIndexerClient {
 		t.Failf("Error opening client to t.ServerPort [%d]: %v", t.ServerPort, err)
 	}
 	return client
+}
+
+func concurrentBruteforceIndexesPackages(clientCounter int, t *TestRun, segmentedPackages [][]*Package) {
+	t.waiting.Add(t.ConcurrencyLevel)
+	for _, p := range segmentedPackages {
+		clientCounter++
+		go func(number int, packagesToProcess []*Package) {
+			name := fmt.Sprintf("client[%d]", number+1)
+			log.Printf("Starting %s", name)
+			defer t.waiting.Done()
+
+			client := makeClient(name, t)
+			defer client.Close()
+
+			err := BruteforceIndexesPackages(client, packagesToProcess)
+			if err != nil {
+				t.Failf("%v", err)
+			}
+		}(clientCounter, p)
+	}
+	t.waiting.Wait()
+}
+
+func concurrentBruteforceRemovesAllPackages(clientCounter int, t *TestRun, segmentedPackages [][]*Package) {
+	t.waiting.Add(t.ConcurrencyLevel)
+	for _, p := range segmentedPackages {
+		clientCounter++
+		go func(number int, packagesToProcess []*Package) {
+			name := fmt.Sprintf("client[%d]", number+1)
+			log.Printf("Starting %s", name)
+			defer t.waiting.Done()
+
+			client := makeClient(name, t)
+			defer client.Close()
+
+			err := BruteforceRemovesAllPackages(client, packagesToProcess)
+			if err != nil {
+				t.Failf("%v", err)
+			}
+		}(clientCounter, p)
+	}
+	t.waiting.Wait()
+}
+
+func concurrentVerifyAllPackages(clientCounter int, t *TestRun, segmentedPackages [][]*Package, expectedRepose ResponseCode) {
+	t.waiting.Add(t.ConcurrencyLevel)
+	for _, p := range segmentedPackages {
+		clientCounter++
+		go func(number int, packagesToProcess []*Package) {
+			name := fmt.Sprintf("client[%d]", number+1)
+			log.Printf("Starting %s", name)
+			defer t.waiting.Done()
+
+			client := makeClient(name, t)
+			defer client.Close()
+
+			err := VerifyAllPackages(client, packagesToProcess, expectedRepose)
+			if err != nil {
+				t.Failf("%v", err)
+			}
+		}(clientCounter, p)
+	}
+	t.waiting.Wait()
+}
+
+func durationInMillis(d time.Duration) int64 {
+	return d.Nanoseconds() / int64(time.Millisecond)
 }
